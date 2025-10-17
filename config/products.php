68eb34c4e6fda9aef26526b342fc5fe4aca8a4e0
SYEILLA <151445182+Syeilla@users.noreply.github.com>
2025-10-17 08:16:13 +0700

    Update product details and URLs in products.php

--- config/products.php
+++ config/products.php
@@ -11,15 +11,45 @@ return [
         ],
         [
             'id'    => 'PD00002',
-            'name'  => 'Teh Botol',
-            'url'   => 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//98/MTA-3541787/sosro_sosro-teh-botol-kotak--330-ml-_full02.jpg',
+            'name'  => 'Teh Kotak',
+            'url'   => 'https://c.alfagift.id/product/1/1_A12790004903_20230911163611621_base.jpg',
             'price' => 15000,
         ],
-         [
+        [
             'id'    => 'PD00003',
             'name'  => 'Mizone',
-            'url'   => 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//98/MTA-3541787/sosro_sosro-teh-botol-kotak--330-ml-_full02.jpg',
+            'url'   => 'https://arti-assets.sgp1.cdn.digitaloceanspaces.com/megaswalayan/products/2cef0c66-4fee-4307-8726-4afa7b721476.jpg',
             'price' => 20000,
-        ]
+        ],
+        [
+            'id'    => 'PD00004',
+            'name'  => 'Milo Kaleng',
+            'url'   => 'https://c.alfagift.id/product/1/1_A8184040002167_20250703112300981_base.jpg',
+            'price' => 9000,
+        ],
+        [
+            'id'    => 'PD00005',
+            'name'  => 'Pocari Sweat',
+            'url'   => 'https://images.tokopedia.net/img/cache/700/VqbcmM/2020/10/3/e77aa0b1-5791-4ed5-ad05-9af9ae3aa301.jpg',
+            'price' => 21000,
+        ],
+        [
+            'id'    => 'PD00006',
+            'name'  => 'Le Minerale',
+            'url'   => 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//99/MTA-3977811/le_minerale_le_minerale_air_mineral_1500ml_full02.jpg',
+            'price' => 8000,
+        ],
+        [
+            'id'    => 'PD00007',
+            'name'  => 'Kratingdaeng',
+            'url'   => 'https://images.tokopedia.net/img/cache/700/VqbcmM/2024/8/1/58929b54-2a48-4b34-b709-6d261d8e3a0e.jpg',
+            'price' => 23000,
+        ],
+        [
+            'id'    => 'PD00008',
+            'name'  => 'Coca Cola',
+            'url'   => 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/catalog-image/MTA-10882042/coca_cola_coca_cola_pet_390_ml_full03_qyi4y7hz.jpeg',
+            'price' => 30000,
+        ],
     ],
 ];
